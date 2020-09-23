0c55eea4aedfab670952143ef3932c6560d3e3b2
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-09-23 23:26:11 +0300

    Додає файли

---
+++ js/autocheck/task-5-autocheck.js
@@ -0,0 +1,36 @@
+// Задача 2-5
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.
+// Если нашли запрещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.
+// Слова в строке могут быть в произвольном регистре.
+
+// ***************************************************************************************************************************
+
+function checkForSpam(str) {
+  'use strict';
+  // Write code under this line
+  const forbiddenWord1 = 'spam';
+  const forbiddenWord2 = 'sale';
+
+  console.log(str.toLowerCase());
+
+  if (
+    str.toLowerCase().includes(forbiddenWord1) ||
+    str.toLowerCase().includes(forbiddenWord2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
---
+++ js/schoology/task-5-schoology.js
@@ -0,0 +1,46 @@
+// Задание 5
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.
+// Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.
+// Слова в строке могут быть в произвольном регистре.
+
+// const checkForSpam = function(message) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+// ***************************************************************************************************************************
+
+const checkForSpam = function (message) {
+  const forbiddenWord1 = 'spam';
+  const forbiddenWord2 = 'sale';
+
+  console.log(message.toLowerCase());
+
+  if (
+    message.toLowerCase().includes(forbiddenWord1) ||
+    message.toLowerCase().includes(forbiddenWord2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
